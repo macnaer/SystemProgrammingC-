f952f096253912967647655a6bb93d2d57c7eaac
trofimchuk <trofimchuk@itstep.academy>
2022-02-14 10:15:23 +0200

    Minrx and Semaphore

---
+++ Mutex/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static Mutex mutex = new Mutex();
+
+    static int counter = 1; 
+
+    public static void Count()
+    {
+        mutex.WaitOne();
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {counter}");
+            counter++;
+            Thread.Sleep(100);
+        }
+        mutex.ReleaseMutex();
+    }
+
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread thread = new Thread(Count);
+            thread.Name = $"Trhead {i}";
+            thread.Start();
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Semaphore/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static Semaphore semaphore = new Semaphore(5, 5);
+    static int counter = 0;
+
+    private static void Count()
+    {
+        semaphore.WaitOne();
+        Interlocked.Increment(ref counter);
+        Console.WriteLine($"{Thread.CurrentThread.Name} Started... Semaphore tread count {counter}");
+        Thread.Sleep(new Random().Next(500, 1200));
+
+        Console.WriteLine($"{Thread.CurrentThread.Name} Finishing... Semaphore tread count {counter}");
+        Interlocked.Decrement(ref counter);
+        semaphore.Release();
+    }
+
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Thread thread = new Thread(Count);
+            thread.Name = $"Tread name {i}";
+            thread.Start();
+        }
+        Console.ReadKey();
+    }
+}
