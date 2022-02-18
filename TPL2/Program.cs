b11e261179d771b9cbda47c9970d37ec715180f7
trofimchuk <trofimchuk@itstep.academy>
2022-02-18 10:05:34 +0200

    TPL 2,3

---
+++ TPL2/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void SimpleTask()
+    {
+        Console.WriteLine($"SimpleTask: CurrentID {Task.CurrentId} is running.");
+        Thread.Sleep(3000);
+        Console.WriteLine($"SimpleTask: CurrentID {Task.CurrentId} is finished.");
+
+    }
+    static void Main()
+    {
+        Console.WriteLine($"Main tread starts.");
+        Task task1 = new Task(SimpleTask);
+        Task task2 = new Task(SimpleTask);
+        task1.Start();
+        task2.Start();
+        Console.WriteLine($"Id task1 {task1.Id}");
+        Console.WriteLine($"Id task2 {task2.Id}");
+        Task.WaitAll(task1, task2);
+        //Task.WaitAny(task1);
+
+        Console.WriteLine($"Main tread finished.");
+    }
+}
---
+++ TPL3/Program.cs
@@ -0,0 +1,32 @@
+﻿struct Calc
+{
+    public int a;
+    public int b;
+}
+
+class Program
+{
+    static int Sum(object obj)
+    {
+        int a = ((Calc)obj).a;
+        int b = ((Calc)obj).b;
+
+        Thread.Sleep(3000);
+
+        return a + b;
+    }
+    static void Main()
+    {
+        Console.WriteLine($"Main tread starts.");
+        Calc calc;
+        calc.a = 10;
+        calc.b = 5;
+        Task<int> task = new Task<int>(Sum, calc);
+
+        task.Start();
+
+        Console.WriteLine($"Sum => {task.Result}");
+
+        Console.WriteLine($"Main tread finished.");
+    }
+}
